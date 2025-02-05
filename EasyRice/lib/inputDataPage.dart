3326c0848eea48fb302a46069a91819593e43b89
Lintang <lintangwaladi@gmail.com>
2025-02-05 10:55:43 +0700

    tambahin validator (mumettt)

--- EasyRice/lib/inputDataPage.dart
+++ EasyRice/lib/inputDataPage.dart
@@ -14,6 +14,7 @@ class Pagehitungtakaran extends StatefulWidget {
 class _PagehitungtakaranState extends State<Pagehitungtakaran> {
   final TextEditingController _beratBerasInput = TextEditingController();
   final TextEditingController _porsiNasiInput = TextEditingController();
+  final _inputKey = GlobalKey<FormState>();
   bool _iskilogram = false;
 
   Map<String, double> hasil = {};
@@ -55,114 +56,132 @@ class _PagehitungtakaranState extends State<Pagehitungtakaran> {
         appBar: navbar(),
         body: Padding(
           padding: const EdgeInsets.all(30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 30),
-              Text(
-                'Hitung Takaran Beras dan Air untuk ${widget.beras['jenis']} ',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+          child: Form(
+            key: _inputKey,
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 30),
+            Text(
+              'Hitung Takaran Beras dan Air untuk ${widget.beras['jenis']} ',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30),
+            TextFormField(
+              controller: _beratBerasInput,
+              decoration: const InputDecoration(
+                labelText: 'Berat beras (gram/kilogram)',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.numberWithOptions(decimal: true, signed: false),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                    if (_porsiNasiInput.text.isEmpty) {
+                      return 'Harap isi kolom input!';
+                    }
+                  } else {
+                    if (double.tryParse(value) == null) {
+                      return 'Input harus berupa angka!';
+                    } else if (double.tryParse(value)! < 0) {
+                      return 'Input tidak boleh kurang dari nol!';
+                    } else if (_porsiNasiInput.text.isNotEmpty) {
+                      return 'Masukkan hanya salah satu (berat beras atau porsi nasi)';
+                    }
+                  }
+                return null;
+              },
+            ),
+            const SizedBox(height: 5),
+             Container(
+              padding: const EdgeInsets.all(12),
+              child: Row(children: [
+                Text(
+                  'gram',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: _beratBerasInput,
-                decoration: const InputDecoration(
-                  labelText: 'Berat beras (gram/kilogram)',
+                SizedBox(
+                  width: 10,
                 ),
+                Switch(
+                    value: _iskilogram,
+                    onChanged: (value) {
+                      setState(() {
+                        _iskilogram = value;
+                      });
+                    }),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'kilogram',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ]),
+            ),
+            const SizedBox(height: 20,),
+            TextFormField(
+              controller: _porsiNasiInput,
+              decoration: const InputDecoration(
+                labelText: 'Porsi Nasi',
+                border: OutlineInputBorder(),
               ),
-              const SizedBox(height: 10),
-               Container(
-                padding: const EdgeInsets.all(12),
-                child: Row(children: [
-                  Text(
-                    'gram',
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Switch(
-                      value: _iskilogram,
-                      onChanged: (value) {
-                        setState(() {
-                          _iskilogram = value;
-                        });
-                      }),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    'kilogram',
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                ]),
-              ),
-              const SizedBox(height: 20,),
-              Center(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green[700],
-                  ),
-                  onPressed: () {
+              keyboardType: TextInputType.numberWithOptions(decimal: true, signed: false),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                    if (_beratBerasInput.text.isEmpty) {
+                      return 'Harap isi kolom input!';
+                    }
+                  } else {
+                    if (double.tryParse(value) == null) {
+                      return 'Input harus berupa angka!';
+                    } else if (double.tryParse(value)! < 0) {
+                      return 'Input tidak boleh kurang dari nol!';
+                    } else if (_beratBerasInput.text.isNotEmpty) {
+                      return 'Masukkan hanya salah satu (berat beras atau porsi nasi)';
+                    }
+                  }
+                return null;
+              },
+            ),
+            SizedBox(height: 20,),
+            Text('*Isi salah satu dari dua kolom diatas'),
+            const SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green[700],
+                ),
+                onPressed: () {
+                  if (_inputKey.currentState!.validate()){
                     hitung();
                     Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            Hasil(hasil: hasil, beras: widget.beras),
-                      ),
-                    );
-                  },
-                  child: const Text(
-                    'Hitung',
-                    style: TextStyle(
-                      color: Colors.white,
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          Hasil(hasil: hasil, beras: widget.beras),
                     ),
+                  );
+                  } 
+            
+                },
+                child: const Text(
+                  'Hitung',
+                  style: TextStyle(
+                    color: Colors.white,
                   ),
                 ),
               ),
-              
-              SizedBox(height: 50,),
-              TextField(
-                controller: _porsiNasiInput,
-                decoration: const InputDecoration(
-                  labelText: 'Porsi nasi yang diinginkan',
-                ),
-              ),
-              
-              const SizedBox(height: 20),
-              Center(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green[700],
-                  ),
-                  onPressed: () {
-                    hitung();
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            Hasil(hasil: hasil, beras: widget.beras),
-                      ),
-                    );
-                  },
-                  child: const Text(
-                    'Hitung',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
+        ))
+          
         ));
   }
 }
